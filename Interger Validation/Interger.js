a4a0956aa31d3c183abcafabc8f052084c2e1023
DevPepperspray <87536433+DevPepperspray@users.noreply.github.com>
2021-09-08 15:19:44 +0100

    Add files via upload

---
+++ Interger Validation/Interger.js
@@ -0,0 +1,21 @@
+function submit()
+{
+
+    var red = document.getElementById("red").value;
+    var white = document.getElementById("whiter").value;
+    var blue = document.getElementById("blue").value;
+
+    let largest;
+
+    if (red > white && red > blue) {
+        largest = red;
+    }
+    else if (white > red && white > blue) {
+        largest = white;
+    }
+    else if (blue > white && blue > red) {
+        largest = blue;
+    }
+
+    alert("The largest number is " + largest);
+}
